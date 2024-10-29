3f12df7c61f82fbc7d153a22674108571d97f911
Franco <francodasilva_@outlook.com>
2024-10-29 11:18:49 -0300

    Agrego controllers y service para gestion de oficinas

---
+++ Gestion_Cartuchos/Controllers/OficinaController.cs
@@ -0,0 +1,51 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Models;
+using Models.DTOs;
+using Services;
+
+namespace Controllers{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class OficinaController(OficinaService oficinaService, IMapper mapper, Gestion_Cartuchos_Context context) : ControllerBase
+    {
+        private readonly OficinaService _oficinaService = oficinaService;
+        private readonly IMapper _mapper = mapper;
+        private readonly Gestion_Cartuchos_Context _context = context;
+
+        [HttpGet]
+        public async Task<IEnumerable<OficinaDTO>> GetAll()
+        {
+            return await _oficinaService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<OficinaDTO> GetById(int id)
+        {
+            return await _oficinaService.GetById(id);
+        }
+
+        [HttpPost]
+        public async Task<Oficina> Create(OficinaDTO oficinaDTO)
+        {
+            var oficina = await _oficinaService.Create(oficinaDTO);
+            return _mapper.Map<Oficina>(oficina);
+        }
+
+        [HttpPut("{id}")]
+        public async Task Update(int id, OficinaDTO oficinaDTO)
+        {
+            await _oficinaService.Update(id, oficinaDTO);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _oficinaService.Delete(id);
+        }
+    }
+}
---
+++ Gestion_Cartuchos/Services/OficinaService.cs
@@ -0,0 +1,59 @@
+using AutoMapper;
+using Models;
+using Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services
+{
+    public class OficinaService
+    {
+        private readonly Gestion_Cartuchos_Context _context;
+        private readonly IMapper _mapper;
+
+        public OficinaService(Gestion_Cartuchos_Context context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<OficinaDTO>> GetAll()
+        {
+            var oficinas = await _context.Oficinas
+            .ToListAsync();
+            return _mapper.Map<IEnumerable<OficinaDTO>>(oficinas);
+        }
+
+        public async Task<OficinaDTO> GetById(int id)
+        {
+            var oficina = await _context.Oficinas
+            .FirstOrDefaultAsync(x => x.Id == id);
+            return _mapper.Map<OficinaDTO>(oficina);
+        }
+
+        public async Task<OficinaDTO> Create(OficinaDTO oficinaDTO)
+        {
+            var oficina = _mapper.Map<Oficina>(oficinaDTO);
+            _context.Oficinas.Add(oficina);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<OficinaDTO>(oficina);
+        }
+
+        public async Task Update(int id, OficinaDTO oficinaDTO)
+        {
+            var oficina = _mapper.Map<Oficina>(oficinaDTO);
+            oficina.Id = id;
+            _context.Entry(oficina).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var oficina = await _context.Oficinas.FirstOrDefaultAsync(x => x.Id == id);
+            _context.Oficinas.Remove(oficina);
+            await _context.SaveChangesAsync();
+        }
+
+        
+
+    }
+}
