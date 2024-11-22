bdba949b96315f6b26422d8697a47f754adc09b3
Franco <francodasilva_@outlook.com>
2024-11-22 12:44:23 -0300

    Agrego relacion con entidad cartucho e impresora

--- Gestion_Cartuchos/Models/DTOs/Asignar_Impresora_DTO.cs
+++ Gestion_Cartuchos/Models/DTOs/Asignar_Impresora_DTO.cs
@@ -4,7 +4,9 @@ public class Asignar_Impresora_DTO
 {
     public int Id { get; set; }
     public required int impresora_id { get; set; }
+    public required Impresora impresora { get; set; }
     public required int cartucho_id { get; set; }
+    public required Cartucho cartucho { get; set; }
     public DateOnly fecha_asignacion { get; set; }
     public DateOnly? fecha_desasignacion { get; set; }
     public string? observaciones { get; set; }
